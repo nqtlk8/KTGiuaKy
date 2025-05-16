c62b95b09cb7062c18758f5b6df38460c3969b80
Trang00001 <lemaitrang000@gmail.com>
2025-05-16 14:48:07 +0700

    FrontEnd

--- KTGiuaKy/Form1.Designer.cs
+++ KTGiuaKy/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace KTGiuaKy
 {
-    partial class Form1
+    partial class fLogin
     {
         /// <summary>
         /// Required designer variable.
@@ -19,7 +19,25 @@
             }
             base.Dispose(disposing);
         }
-
+        /*
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // fLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 261);
+            this.Name = "fLogin";
+            this.ResumeLayout(false);
+        }
+        */
+        private System.Windows.Forms.TextBox txtAcc;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtPass;
+        private System.Windows.Forms.Button btnLogin;
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -28,10 +46,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtAcc = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAcc
+            // 
+            this.txtAcc.Location = new System.Drawing.Point(148, 70);
+            this.txtAcc.Name = "txtAcc";
+            this.txtAcc.Size = new System.Drawing.Size(162, 20);
+            this.txtAcc.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(148, 122);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tên tài khoản:";
+            // 
+            // txtPass
+            // 
+            this.txtPass.AutoSize = true;
+            this.txtPass.Location = new System.Drawing.Point(71, 125);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(55, 13);
+            this.txtPass.TabIndex = 3;
+            this.txtPass.Text = "Mật khẩu:";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(174, 189);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // fLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(414, 261);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtAcc);
+            this.Name = "fLogin";
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
